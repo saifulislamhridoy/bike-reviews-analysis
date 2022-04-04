121f6677238dab32162547d5217a7ea8464a72f0
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-04-05 03:00:40 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Dashboard from './components/Dashboard/Dashboard';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <h2>hello</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <h4>404</h4>
+        </div>
+    );
+};
+
+export default NotFound;
