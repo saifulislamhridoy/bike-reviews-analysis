8fe2d12d73a877dac2510d61fac9638fdbb7fe0e
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-04-05 19:33:06 +0600

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-400'>
+            <p>All Right Reserved By <span className='text-green'>Dreams Bike</span></p>
+        </div>
+    );
+};
+
+export default Footer;
