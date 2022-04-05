f4b70483a0364fcc8c079ce85601fd50e9de43fe
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-04-05 22:53:40 +0600

    complete all

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 https://shiny-cannoli-939be8.netlify.app/
 
-* Review website
+* This is customar reviews website. i will create this website with react and tailwind.i will created home page, reviews page, blogs page and about page.
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found</h2>
-            <h4>404</h4>
+        <div className='h-[70vh] text-center mt-20'>
+            <h2 className='text-5xl font-bold'>Not Found</h2>
+            <h4 className='text-2xl font-bold'>404</h4>
         </div>
     );
 };
